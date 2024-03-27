105cc1a5ab2536698aa803594d26d1326d6014ba
EvgrafovMichail <evgrafovmd@gmail.com>
2024-03-28 02:02:40 +0300

    Add data migration functions

--- sqlite_to_postgres/run_tests.py
+++ sqlite_to_postgres/run_tests.py
@@ -1,7 +1,10 @@
 import os
 
-from db_tools.save_load_utils import transfer_data
 from models.db_models import db_model_mapping
+from tests.testing_utils import (
+    test_tables_content,
+    test_tables_size,
+)
 
 
 def main() -> None:
@@ -11,7 +14,12 @@ def main() -> None:
     path_to_postgres = os.path.join("connection_config.json")
     assert os.path.exists(path_to_postgres)
 
-    transfer_data(
+    test_tables_size(
+        path_to_postgres,
+        path_to_sqlite,
+        db_model_mapping,
+    )
+    test_tables_content(
         path_to_postgres,
         path_to_sqlite,
         db_model_mapping,
